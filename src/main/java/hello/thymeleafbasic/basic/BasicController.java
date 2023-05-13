85e0543a9c9ccbd56e7353d89492ab4236edb335
kimjunoh <kimjo8317@gmail.com>
2023-05-14 00:14:08 +0900

    <th:block th:each="">기능 <div>가 2개이상 렌더링될시 반복기능

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -120,6 +120,11 @@ public class BasicController {
     public String comments(Model model) {
         model.addAttribute("data", "Spring!");
         return "basic/comments";
+    }
 
+    @GetMapping("/block")
+    public String bolck(Model model) {
+        addUsers(model);
+        return "/basic/block";
     }
 }
